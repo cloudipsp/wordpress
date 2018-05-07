363a9af8a15c321e1b64f15c076cfcec77f1af67
dmiroshnikov <dmmatrix222@gmail.com>
2018-05-07 12:08:44 +0300

    some fix

--- Paid_Membership_Pro/pmpro-fondy-gateway/services/fondy-ins.php
+++ Paid_Membership_Pro/pmpro-fondy-gateway/services/fondy-ins.php
@@ -1,115 +1,128 @@
 <?php
 //in case the file is loaded directly
-
-if (!defined("ABSPATH")) {
-    global $isapage;
-    $isapage = true;
-
-    define('WP_USE_THEMES', false);
-    require_once(dirname(__FILE__) . '/../../../../wp-load.php');
-}
 require_once(dirname(__FILE__) . '/../classes/fondy.lib.php');
 
-define('PMPRO_FONDY_DEBUG', false);
+/**
+ * DEBUG mode
+ */
+define('FONDY_PMPRO_DEBUG', "log");
 
 if (empty($_POST)) {
-    $fap = json_decode(file_get_contents("php://input"));
-    if (empty($fap)) {
-        die('no response');
+    $callback = json_decode(file_get_contents("php://input"));
+    if (empty($callback)) {
+        pmpro_fondy_fnlog("Empty responce.");
+        pmpro_fondy_exit();
     }
     $_POST = array();
-    foreach ($fap as $key => $val) {
-        $_POST[$key] = $val;
+    foreach ($callback as $key => $val) {
+        $_POST[esc_sql($key)] = esc_sql($val);
     }
 }
-$base64_data = $_POST['data'];
-$sign = $_POST['signature'];
-$_POST = json_decode(base64_decode($_POST['data']), true)['order'];
 
-//some globals
-global $wpdb, $gateway_environment, $logstr;
-$logstr = "";    //will put debug info here and write to fnlog.txt
-
-//validate?
-if (pmpro_fondyValidate($base64_data, $sign) !== true) {
-    pmpro_fondyExit();
+$base64_data = esc_sql($_POST['data']);
+$signature = esc_sql((string)$_POST['signature']);
+$response = json_decode(base64_decode(esc_sql($_POST['data'])), true)['order'];
+if (empty($response)) {
+    pmpro_fondy_fnlog("Empty response.");
+    pmpro_fondy_exit();
 }
 
+//some globals
+global $wpdb, $gateway_environment, $fondy_logstr;
+$fondy_logstr = "";    //will put debug info here and write to pmpro_fondy_fnlog.txt
+
+/**
+ * Validate Fondy answer
+ */
+if (pmpro_fondy_validate_sign($base64_data, $signature, $response) !== true) {
+    pmpro_fondy_fnlog("Signature Invalid.");
+    pmpro_fondy_exit();
+}
 //assign posted variables to local variables
-$amount = pmpro_getParam('amount', 'POST');
-$signature = pmpro_getParam('signature', 'POST');
-$subscr_id = pmpro_getParam('product_id', 'POST');
-$rectoken = pmpro_getParam('rectoken', 'POST');
-$order_id = explode('#', pmpro_getParam('order_id', 'POST'))[0];
-$customer_email = pmpro_getParam('sender_email', 'POST');
-$system_payment_id = pmpro_getParam('payment_id', 'POST');
-$order_status = pmpro_getParam('order_status', 'POST');
+$amount = (int)$response['amount'];
+$actual_amount = (int)$response['actual_amount'];
+$subscr_id = (int)$response['product_id'];
+$rectoken = esc_sql((string)$response['rectoken']);
+$order_id = explode('#', esc_sql((string)$response['order_id']))[0];
+$customer_email = esc_sql((string)$response['sender_email']);
+$system_payment_id = (int)$response['payment_id'];
+$order_status = esc_sql((string)$response['order_status']);
 
 if ($order_status == FondyForm::ORDER_APPROVED) {
-    $recurring = pmpro_getParam('rectoken', 'POST');
 
     $morder = new MemberOrder($order_id);
     $morder->getMembershipLevel();
 
     $morder->getUser();
-    if (isset($_POST['rectoken'])) {
+    if (isset($rectoken) and !empty($rectoken)) {
         $id = $morder->id;
-        $rec = $wpdb->query("UPDATE `$wpdb->pmpro_membership_orders` SET fondy_token = '" . $_POST['rectoken'] . "' WHERE id = " . $id . "");
-
+        $rec = $wpdb->query($wpdb->prepare("
+            UPDATE `$wpdb->pmpro_membership_orders`
+            SET `fondy_token` = %s
+            WHERE id = %d",
+            array($rectoken, $id)
+        ));
     }
 
-    fnlog("ORDER_CREATED: ORDER: " . var_export($morder, true) . "\n---\n");
+    pmpro_fondy_fnlog("ORDER_CREATED: ORDER: " . var_export($morder, true) . "\n---\n");
 
     if (!empty ($morder) && !empty ($morder->status) && $morder->status == 'success') {
         $last_subscr_order = new MemberOrder();
-        $last_subscr_order->getLastMemberOrderBySubscriptionTransactionID($_POST['payment_id']);
-        if (isset($_POST['parent_order_id'])) {
-            $order_old_id = $_POST['parent_order_id'];
+        $last_subscr_order->getLastMemberOrderBySubscriptionTransactionID($system_payment_id);
+        if (isset($response['parent_order_id'])) {
+            $order_old_id = esc_sql((string)$response['parent_order_id']);
         } else {
-            $order_old_id = $_POST['order_id'];
+            $order_old_id = $order_id;
         }
-        pmpro_FondyinsSaveOrder($order_old_id, $last_subscr_order, $_POST);
-        fnlog("Checkout was already processed (" . $morder->code . "). Reccuring this request.");
+        $order_info = array(
+            'amount' => $amount,
+            'actual_amount' => $actual_amount,
+            'order_id' => $order_id,
+            'payment_id' => $system_payment_id,
+        );
+        pmpro_fondy_insSaveOrder($order_old_id, $last_subscr_order, $order_info);
+        pmpro_fondy_fnlog("Checkout was already processed (" . $morder->code . "). Reccuring this request.");
 
-    } elseif (pmpro_insChangeMembershipLevel($order_id, $morder, $_POST)) {
-        fnlog("Checkout processed (" . $morder->code . ") success!");
+    } elseif (pmpro_fondy_insChangeMembershipLevel($order_id, $morder, $system_payment_id, $rectoken)) {
+
+        pmpro_fondy_fnlog("Checkout processed (" . $morder->code . ") success!");
     } else {
-        fnlog("ERROR: Couldn't change level for order (" . $morder->code . ").");
+        pmpro_fondy_fnlog("ERROR: Couldn't change level for order (" . $morder->code . ").");
     }
-    pmpro_fondyExit(pmpro_url("confirmation", "?level=" . $morder->membership_level->id));
+    pmpro_fondy_exit(pmpro_url("confirmation", "?level=" . $morder->membership_level->id));
 } else {
-    fnlog("ERROR: (" . $order_status . ").");
+    pmpro_fondy_fnlog("ERROR: (" . $order_status . ").");
 }
 
 
-function fnlog($s)
+function pmpro_fondy_fnlog($s)
 {
-    global $logstr;
-    $logstr .= "\t" . $s . "\n";
+    global $fondy_logstr;
+    $fondy_logstr .= "\t" . $s . "\n";
 }
 
-function pmpro_fondyExit($redirect = false)
+function pmpro_fondy_exit($redirect = false)
 {
-    global $logstr;
+    global $fondy_logstr;
 
-    $logstr = var_export($_REQUEST, true) . "Logged On: " . date("m/d/Y H:i:s") . "\n" . $logstr . "\n-------------\n";
+    $fondy_logstr = var_export($_REQUEST, true) . "Logged On: " . date("m/d/Y H:i:s") . "\n" . $fondy_logstr . "\n-------------\n";
 
     //log in file or email?
-    if (defined('PMPRO_FONDY_DEBUG') && PMPRO_FONDY_DEBUG === "log") {
+    if (defined('FONDY_PMPRO_DEBUG') && FONDY_PMPRO_DEBUG === "log") {
         //file
         $loghandle = fopen(dirname(__FILE__) . "/../logs/fondy.txt", "a+");
-        fwrite($loghandle, $logstr);
+        fwrite($loghandle, $fondy_logstr);
         fclose($loghandle);
-    } elseif (defined('PMPRO_FONDY_DEBUG')) {
+    } elseif (defined('FONDY_PMPRO_DEBUG')) {
         //email
-        if (strpos(PMPRO_FONDY_DEBUG, "@")) {
-            $log_email = PMPRO_FONDY_DEBUG;
+        if (strpos(FONDY_PMPRO_DEBUG, "@")) {
+            $log_email = FONDY_PMPRO_DEBUG;
         }    //constant defines a specific email address
         else {
             $log_email = get_option("admin_email");
         }
 
-        wp_mail($log_email, get_option("blogname") . " Fondy log", nl2br($logstr));
+        wp_mail($log_email, get_option("blogname") . " Fondy log", nl2br($fondy_logstr));
     }
 
     if (!empty($redirect)) {
@@ -119,14 +132,14 @@ function pmpro_fondyExit($redirect = false)
     exit;
 }
 
-function pmpro_fondyValidate($base64_data, $sign)
+function pmpro_fondy_validate_sign($base64_data, $sign, $response = array())
 {
 
     $settings = array(
         'merchant_id' => pmpro_getOption("fondy_merchantid"),
         'secret_key' => pmpro_getOption("fondy_securitykey"),
     );
-    $validated = FondyForm::isPaymentValid($settings, $_POST, $base64_data, $sign);
+    $validated = FondyForm::isPaymentValid($settings, $response, $base64_data, $sign);
 
 
     if ($validated !== true) {
@@ -137,9 +150,9 @@ function pmpro_fondyValidate($base64_data, $sign)
 
 }
 
-function pmpro_insChangeMembershipLevel($txn_id, &$morder, $data)
+function pmpro_fondy_insChangeMembershipLevel($txn_id, &$morder, $payment_id, $rectoken)
 {
-    $recurring = pmpro_getParam('rectoken', 'POST');
+    $recurring = $rectoken;
     //filter for level
     $morder->membership_level = apply_filters("pmpro_inshandler_level", $morder->membership_level, $morder->user_id);
 
@@ -148,10 +161,8 @@ function pmpro_insChangeMembershipLevel($txn_id, &$morder, $data)
 
     //fix expiration date
     if (!empty($morder->membership_level->expiration_number)) {
-
         $enddate = "'" . date("Y-m-d", strtotime("+ " . $morder->membership_level->expiration_number . " " . $morder->membership_level->expiration_period, current_time("timestamp"))) . "'";
     } else {
-
         $enddate = "NULL";
     }
 
@@ -187,7 +198,7 @@ function pmpro_insChangeMembershipLevel($txn_id, &$morder, $data)
 
     global $pmpro_error, $wpdb;
     if (!empty($pmpro_error)) {
-        fnlog($pmpro_error);
+        pmpro_fondy_fnlog($pmpro_error);
     }
 
     if (pmpro_changeMembershipLevel($custom_level, $morder->user_id) !== false) {
@@ -195,7 +206,7 @@ function pmpro_insChangeMembershipLevel($txn_id, &$morder, $data)
         $morder->status = "success";
         $morder->payment_transaction_id = $txn_id;
         if ($recurring) {
-            $morder->subscription_transaction_id = $data['payment_id'];
+            $morder->subscription_transaction_id = $payment_id;
         } else {
             $morder->subscription_transaction_id = '';
         }
@@ -203,7 +214,7 @@ function pmpro_insChangeMembershipLevel($txn_id, &$morder, $data)
 
         //add discount code use
         if (!empty($discount_code) && !empty($use_discount_code)) {
-            $wpdb->query("INSERT INTO $wpdb->pmpro_discount_codes_uses (code_id, user_id, order_id, timestamp) VALUES('" . $discount_code_id . "', '" . $morder->user_id . "', '" . $morder->id . "', '" . current_time('mysql') . "')");
+            $wpdb->query("INSERT INTO $wpdb->pmpro_discount_codes_uses (code_id, user_id, order_id, timestamp) VALUES('" . esc_sql($discount_code_id) . "', '" . esc_sql($morder->user_id) . "', '" . esc_sql($morder->id) . "', '" . current_time('mysql') . "')");
         }
 
 
@@ -217,7 +228,7 @@ function pmpro_insChangeMembershipLevel($txn_id, &$morder, $data)
             $invoice = null;
         }
 
-        fnlog("CHANGEMEMBERSHIPLEVEL: ORDER: " . var_export($morder, true) . "\n---\n");
+        pmpro_fondy_fnlog("CHANGEMEMBERSHIPLEVEL: ORDER: " . var_export($morder, true) . "\n---\n");
 
         $user = get_userdata($morder->user_id);
 
@@ -240,7 +251,7 @@ function pmpro_insChangeMembershipLevel($txn_id, &$morder, $data)
     }
 }
 
-function pmpro_FondyinsSaveOrder($txn_id, $last_order, $data)
+function pmpro_fondy_insSaveOrder($txn_id, $last_order, $order_info = array())
 {
     global $wpdb;
 
@@ -257,14 +268,14 @@ function pmpro_FondyinsSaveOrder($txn_id, $last_order, $data)
         $morder->user_id = $last_order->user_id;
         $morder->membership_id = $last_order->membership_id;
 
-        if (isset($_POST['amount'])) {
-            $morder->subtotal = $_POST['amount'] / 100;
-        } elseif (isset($_POST['actual_amount'])) {
-            $morder->total = (!empty($_POST['amount']) ? $_POST['amount'] / 100 : 0);
+        if (isset($order_info['amount'])) {
+            $morder->subtotal = $order_info['amount'] / 100;
+        } elseif (isset($order_info['actual_amount'])) {
+            $morder->total = (!empty($order_info['amount']) ? $order_info['amount'] / 100 : 0);
         }
 
-        $morder->payment_transaction_id = $_POST['order_id'];
-        $morder->subscription_transaction_id = $_POST['payment_id'];
+        $morder->payment_transaction_id = $order_info['order_id'];
+        $morder->subscription_transaction_id = $order_info['payment_id'];
 
         $morder->gateway = $last_order->gateway;
         $morder->gateway_environment = $last_order->gateway_environment;
@@ -286,13 +297,12 @@ function pmpro_FondyinsSaveOrder($txn_id, $last_order, $data)
         $pmproemail = new PMProEmail();
         $pmproemail->sendInvoiceEmail(get_userdata($last_order->user_id), $morder);
 
-        fnlog("New order (" . $morder->code . ") created.");
+        pmpro_fondy_fnlog("New order (" . $morder->code . ") created.");
         do_action('pmpro_subscription_payment_completed', $morder);
 
         return $morder->code;
     } else {
-        fnlog("Duplicate Transaction ID: " . $txn_id);
-
+        pmpro_fondy_fnlog("Duplicate Transaction ID: " . $txn_id);
         return false;
     }
 }
