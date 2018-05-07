ea114ada767bfcb7057752a79033840e2e589823
dmiroshnikov <dmmatrix222@gmail.com>
2018-05-07 12:09:58 +0300

    fix

--- Paid_Membership_Pro/pmpro-fondy-gateway/services/services.php
+++ Paid_Membership_Pro/pmpro-fondy-gateway/services/services.php
@@ -7,8 +7,9 @@
 */
 function pmpro_wp_ajax_fondy_ins()
 {
-	require_once(dirname(__FILE__) . "/fondy-ins.php");	
-	exit;	
+    require_once(dirname(__FILE__) . "/fondy-ins.php");
+    exit;
 }
+
 add_action('wp_ajax_nopriv_fondy-ins', 'pmpro_wp_ajax_fondy_ins');
 add_action('wp_ajax_fondy-ins', 'pmpro_wp_ajax_fondy_ins');
