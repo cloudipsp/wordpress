ea2cc1398e799ed79ae5bb074683713d5ec2130a
dmiroshnikov <dmmatrix222@gmail.com>
2018-05-08 13:30:39 +0300

    update

--- Restrict_Content_Pro/fondy-restrict-gateway/fondy/fondy-admin.php
+++ Restrict_Content_Pro/fondy-restrict-gateway/fondy/fondy-admin.php
@@ -14,70 +14,100 @@ function rcp_fondy_admin_menu()
 
 function rcp_fondy_sanitize_settings($data)
 {
-    return $data;
+    if (empty($data['fondy_merchant_id']) or !is_numeric($data['fondy_merchant_id'])) {
+        add_settings_error(
+            'fondy-rcp',
+            esc_attr('settings_updated'),
+            __('Merchant ID incorrect', 'fondy_rcp'),
+            'error'
+        );
+        return false;
+    }
+    if (empty($data['fondy_secret'])) {
+        add_settings_error(
+            'fondy-rcp',
+            esc_attr('settings_updated'),
+            __('Secret key can\'t be empty!', 'fondy_rcp'),
+            'error'
+        );
+        return false;
+    }
+    add_settings_error(
+        'myUniqueIdentifier',
+        esc_attr('settings_updated'),
+        __('Successfully saved', 'fondy_rcp'),
+        'updated'
+    );
+    $return = array(
+        'fondy_merchant_id' => (int)$data['fondy_merchant_id'],
+        'fondy_secret' => (string)$data['fondy_secret'],
+        'fondy_reccuring' => (bool)$data['fondy_reccuring']
+    );
+    return $return;
 }
 
 function rcp_fondy_admin_page()
 {
     global $rcp_fondy_options;
     ?>
-    <?php if (isset($_REQUEST['settings-updated']) ) : ?>
-    <div class="updated fade"><p><strong><?php _e( 'Options saved', 'rcp' ); ?></strong></p></div>
-    <?php endif; ?>
     <?php if (function_exists('pw_rcp_register_fondy_gateway')) : ?>
+    <?php settings_errors(); ?>
     <form method="post" action="options.php" class="rcp_options_form">
         <?php settings_fields('rcp_fondy_settings_group'); ?>
         <div class="tab_content" id="payments">
             <table class="form-table">
                 <tr valign="top">
                     <th colspan=2>
-                        <h3><?php _e('Fondy Settings', 'rcp'); ?></h3>
+                        <h3><?php _e('Fondy Settings', 'fondy_rcp'); ?></h3>
                     </th>
                 </tr>
 
                 <tr>
                     <th>
-                        <label for="rcp_fondy_settings[fondy_merchant_id]"><?php _e('Fondy MID', 'rcp'); ?></label>
+                        <label
+                            for="rcp_fondy_settings[fondy_merchant_id]"><?php _e('Fondy MID', 'fondy_rcp'); ?></label>
                     </th>
                     <td>
                         <input class="regular-text" id="rcp_fondy_settings[fondy_merchant_id]" style="width: 300px;"
                                name="rcp_fondy_settings[fondy_merchant_id]"
                                value="<?php if (isset($rcp_fondy_options['fondy_merchant_id'])) {
-                                   echo $rcp_fondy_options['fondy_merchant_id'];
+                                   echo esc_attr($rcp_fondy_options['fondy_merchant_id']);
                                } ?>"/>
-                        <p class="description"><?php _e('Enter your Merchant ID', 'rcp'); ?></p>
+                        <p class="description"><?php _e('Enter your Merchant ID', 'fondy_rcp'); ?></p>
                     </td>
                 </tr>
                 <tr>
                     <th>
-                        <label for="rcp_fondy_settings[fondy_secret]"><?php _e('Fondy Secret Key', 'rcp'); ?></label>
+                        <label
+                            for="rcp_fondy_settings[fondy_secret]"><?php _e('Fondy Secret Key', 'fondy_rcp'); ?></label>
                     </th>
                     <td>
                         <input class="regular-text" id="rcp_fondy_settings[stripe_test_secret]" style="width: 300px;"
                                name="rcp_fondy_settings[fondy_secret]"
                                value="<?php if (isset($rcp_fondy_options['fondy_secret'])) {
-                                   echo $rcp_fondy_options['fondy_secret'];
+                                   echo esc_attr($rcp_fondy_options['fondy_secret']);
                                } ?>"/>
-                        <p class="description"><?php _e('Enter your secret key.', 'rcp'); ?></p>
+                        <p class="description"><?php _e('Enter your secret key.', 'fondy_rcp'); ?></p>
                     </td>
                 </tr>
                 <tr>
                     <th>
-                        <label for="rcp_fondy_settings[fondy_reccuring]"><?php _e('Fondy Subscription', 'rcp'); ?></label>
+                        <label
+                            for="rcp_fondy_settings[fondy_reccuring]"><?php _e('Fondy Subscription', 'fondy_rcp'); ?></label>
                     </th>
                     <td>
                         <input class="checkbox" type="checkbox" id="rcp_fondy_settings[fondy_reccuring]"
                                style="width: 15px;"
                                name="rcp_fondy_settings[fondy_reccuring]"
                                value="1" <?php if (isset($rcp_fondy_options['fondy_reccuring'])) checked('1', $rcp_fondy_options['fondy_reccuring']); ?>/>
-                        <p class="description"><?php _e('Enable Subscription', 'rcp'); ?></p>
+                        <p class="description"><?php _e('Enable Subscription', 'fondy_rcp'); ?></p>
                     </td>
                 </tr>
 
             </table>
         </div>
         <p class="submit">
-            <input type="submit" class="button-primary" value="<?php _e('Save Options', 'rcp'); ?>"/>
+            <input type="submit" class="button-primary" value="<?php _e('Save Options', 'fondy_rcp'); ?>"/>
         </p>
     </form>
 <?php endif; ?>
--- Restrict_Content_Pro/fondy-restrict-gateway/index.php
+++ Restrict_Content_Pro/fondy-restrict-gateway/index.php
@@ -1,29 +1,36 @@
 <?php
-/**
- * Plugin Name: z-fondy
- * Description: fondy payment.
- * Version: 1.0.0
- */
+/*
+Plugin Name: Restrict Content Pro - Fondy payment gateway
+Plugin URI: https://fondy.eu
+Description: Fondy Payment Gateway for Restrict Content.
+Version: 1.0.0
+Author: Dmitriy Miroshnikov
+Author URI: https://fondy.eu/
+Domain Path: /languages
+Text Domain: fondy_rcp
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
 define("RCP_FONDY_DIR", dirname(__FILE__));
 //load payment gateway class
-function pw_rcp_register_fondy_gateway( $gateways ) {
+add_filter('rcp_payment_gateways', 'pw_rcp_register_fondy_gateway', 0);
+add_action('plugins_loaded', 'pw_rcp_register_fondy_mainClass', 1);
+
+function pw_rcp_register_fondy_gateway($gateways)
+{
 
     $gateways['Fondy'] = array(
-        'label'        => 'Fondy',
-        'admin_label'  => 'Fondy',
-        'class'        => 'RCP_Payment_Gateway_Fondy'
+        'label' => 'Fondy',
+        'admin_label' => 'Fondy',
+        'class' => 'RCP_Payment_Gateway_Fondy'
     );
     return $gateways;
 }
-add_filter( 'rcp_payment_gateways', 'pw_rcp_register_fondy_gateway',0);
-
-
-add_action('plugins_loaded', 'pw_rcp_register_fondy_mainClass', 1);
 
 function pw_rcp_register_fondy_mainClass()
 {
     global $rcp_fondy_options;
-    $rcp_fondy_options = get_option( 'rcp_fondy_settings' );
+    $rcp_fondy_options = get_option('rcp_fondy_settings');
     require_once(RCP_FONDY_DIR . "/fondy/fondy.php");
     require_once(RCP_FONDY_DIR . "/fondy/fondy-admin.php");
 }
